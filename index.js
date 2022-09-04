44faaa9263af3d892669c9ed40d5810080ae9d41
hernanpm101 <hernanpm101@hotmail.com>
2022-09-04 07:13:40 -0300

    agrego JSON

---
+++ index.js
@@ -0,0 +1,15 @@
+const objetoVacio = {
+
+}
+
+const objetoJson = {
+    "nombreLibro": "El hacedor",
+    "precio": 500,
+    "seVendio": true,
+    "biblioteca": ["suspenso", "40 unidades", "adultos"]
+
+
+}
+
+console.log(objetoJson)
+console.log(JSON.stringify(objetoJson))
